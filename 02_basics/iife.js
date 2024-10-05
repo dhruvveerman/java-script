27ac759fd47ca42f842e14ab023b1a7f330dea20
Dhruv Chaudhary <144075631+dhruvveerman@users.noreply.github.com>
2024-10-05 09:08:23 +0000

    arrow and iife function

---
+++ 02_basics/iife.js
@@ -0,0 +1,8 @@
+(function blue(){
+    console.log(`database is important for backend services`);
+}) ();
+
+( (name) =>{
+    console.log(` ${name}, database is important for backend services`);
+}) ("dhruv")
+
